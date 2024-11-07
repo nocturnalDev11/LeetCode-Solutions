707e87bfc54612b4792662e3beeb922f94624f65
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-11-07 22:57:05 +0800

    Create 174-dungeon-game.js

---
+++ JS/174-dungeon-game.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} dungeon
+ * @return {number}
+ */
+var calculateMinimumHP = function(dungeon) {
+    const m = dungeon.length;
+    const n = dungeon[0].length;
+    
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(Infinity));
+    
+    dp[m][n - 1] = dp[m - 1][n] = 1;
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            const minHealthNeeded = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+            dp[i][j] = Math.max(minHealthNeeded, 1);
+        }
+    }
+    
+    return dp[0][0];
+};
